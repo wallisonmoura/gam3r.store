71e89a4e8ab88bd74c41d3c048b70f7e615e74b5
wallisonmoura <wallison_cedaspy@hotmail.com>
2024-10-05 02:15:47 -0300

    desenvolvimento do monorepo para compartilhamento de código de backend para frontend e mobile utilizando o turboRepo para gerenciamento do projeto

---
+++ apps/frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Montserrat } from 'next/font/google';
+
+export const metadata: Metadata = {
+  title: "Gam3r.store",
+  description: "Versão completa da loja Gam3r.store",
+};
+
+const font = Montserrat({
+  subsets: ['latin']
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
